591c71562db466998abf89a043cc9c28ce31abf3
Jfrattin <106035011+Jfrattin@users.noreply.github.com>
2023-07-07 14:31:31 -0300

    Agrego cambios

--- app/src/main/java/com/example/myapplication/MainActivity.kt
+++ app/src/main/java/com/example/myapplication/MainActivity.kt
@@ -7,12 +7,20 @@ import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.view.Window
 import android.view.WindowManager
+import androidx.cardview.widget.CardView
+import androidx.core.content.ContextCompat
 
 class MainActivity : AppCompatActivity()
 {
 
+    private var isMaleSelected:Boolean = false
+    private var isFemaleSelected:Boolean = true
+
+    private lateinit var viewMale:CardView
+    private lateinit var viewFemale:CardView
 
     override fun onCreate(savedInstanceState: Bundle?) {
+
 
 
 
@@ -24,9 +32,49 @@ class MainActivity : AppCompatActivity()
 
         //Block app mode Lock Task
         this.startLockTask();
-
         setContentView(R.layout.activity_main)
+        // init component
+        iniComponents()
+        // Create Click
+        initListeners()
+
     }
-    
+
+    private fun iniComponents() {
+        viewMale = findViewById(R.id.viewMale)
+        viewFemale = findViewById(R.id.viewFemale)
+    }
+
+    private fun initListeners() {
+            viewMale.setOnClickListener{
+                setGenderColor()
+                changeGender()
+            }
+            viewFemale.setOnClickListener{
+                setGenderColor()
+                changeGender()
+
+            }
+    }
+
+    //function change backgraoud color
+    private fun setGenderColor(){
+            viewMale.setCardBackgroundColor(getBackgraoundColor(isMaleSelected))
+            viewFemale.setCardBackgroundColor(getBackgraoundColor(isFemaleSelected))
+    }
+
+    private fun changeGender(){
+        isMaleSelected = !isMaleSelected
+        isFemaleSelected= !isFemaleSelected
+
+    }
+    private fun getBackgraoundColor(isSelectedComponent: Boolean) :Int {
+        val colorReference = if (isSelectedComponent) {
+            R.color.background_Component
+        } else {
+            R.color.background_Component_selected
+        }
+        return ContextCompat.getColor(this, colorReference)
+    }
 
 }
